667723acea987f98ac85d793a5731e6b6540aef6
Admin <admin@MacBook-Air-cua-Admin.local>
2024-07-04 11:03:27 +0700

    MVC P1

--- src/server.js
+++ src/server.js
@@ -1,27 +1,15 @@
-const express = require('express')
-const path = require('path')
 require('dotenv').config()
-
+const express = require('express')
+const configViewEngine = require('./config/viewEngine')
+const webRoutes = require('./routes/web')
 const app = express()
 const port = process.env.PORT
 const hostname = process.env.HOST_NAME
 
 // config template engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('views engine', 'ejs') 
-
-// config static files
-app.use(express.static(path.join(__dirname, 'public')))
-
-// Khai bao route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/hoidanit', (req, res) => {
-    res.render('sample.ejs')
-})
-
+configViewEngine(app)
+// khai bao route
+app.use('/', webRoutes)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
