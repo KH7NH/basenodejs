83a0981692d9aefa9144761c7dde994398e08b3a
Admin <admin@MacBook-Air-cua-Admin.local>
2024-07-08 02:38:31 +0700

    List User

--- src/ controllers/homeController.js
+++ src/ controllers/homeController.js
@@ -1,27 +1,32 @@
 const connection = require('../config/database')
-const getHomepage = (req, res) => {
-    return res.render('home.ejs')
+const { GetAllUsers } = require('../services/CRUDService')
+const getHomepage = async (req, res) => {
+    let results = await GetAllUsers()
+    return res.render('home.ejs', {listUsers: results})
 }
 
 const getHoidanit = (req, res) => {
     res.render('sample.ejs')
 }
-const postCreateUser = (req, res) => {
-    res.send('create a new user succeed!')
+const postCreateUser = async (req, res) => {
     // let email = req.body.email
     // let name = req.body.name
     // let city = req.body.city
 
     let {email, name, city} = req.body
 
-    connection.query(
-        `INSERT INTO Users (email, name, city)
-        VALUE (?, ?, ?)`,
-        [email, name, city]
+    let [results, fields] = await connection.query(
+        `INSERT INTO Users (email, name, city)VALUE (?, ?, ?)`,[email, name, city]
     )
+    res.send('create a new user succeed!')
+    // const [results, fields] = await connection.query('Select * from Users u')
+}
+const getCreatePage = (req,res) => {
+    res.render('create.ejs')
 }
 module.exports = {
     getHomepage,
     getHoidanit,
-    postCreateUser
+    postCreateUser,
+    getCreatePage
 }
