958f1045e27629560aa44e4a1dc5e82ec71ec830
Admin <admin@MacBook-Air-cua-Admin.local>
2024-07-08 14:08:51 +0700

    Update User

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -4,6 +4,21 @@ const GetAllUsers = async () => {
     let [results, fields] = await connection.query('select * from Users')
     return results
 }
+const getUserById = async (userId) => {
+    let [results, fields] = await connection.query('select * from Users where id = ?', [userId])
+    let user = results && results.length > 0 ? results[0] : {}
+    return user
+}
+const updateUserById = async (email, city, name, userId) => {
+    let [results, fields] = await connection.query(
+        `UPDATE Users
+        SET email = ?, city = ?, name = ?
+        WHERE id = ?
+        `, [email, city, name, userId]
+    )
+}
 module.exports = {
-    GetAllUsers
+    GetAllUsers,
+    getUserById,
+    updateUserById
 }
